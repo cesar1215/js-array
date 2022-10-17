9e2ac0ffa354796026704fc86b77cf5f2bd5f793
cesar1215 <cenriquez@angloaleman.edu.mx>
2022-10-17 10:52:42 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//crea un array(arreglo) listaDeCompras
+//Agrega al arreglo los valores Leche, Pan , Carne, Huevo, Sopa
+let listaDeCompras = ["Leche", "Pan", "Carne", "Huevo", "Sopa"];
+
+//Muestra en la consola el arreglo
+console.log(listaDeCompras);
+
+//Muestra en la consola son el valor Huevo
+console.log(listaDeCompras[3]);
+
